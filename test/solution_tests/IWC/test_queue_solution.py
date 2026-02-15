1a0ee64903869e005aa6a37b7deddd44b68155bb
sbx_user1051 <sbx_user1051@169.254.65.67>
2026-02-15 22:40:39 +0000

    Sun Feb 15 22:40:39 UTC 2026

--- test/solution_tests/IWC/test_queue_solution.py
+++ test/solution_tests/IWC/test_queue_solution.py
@@ -114,3 +114,14 @@ def test_resolve_deduplication_flow_replace_old_item_with_dependencies_changing_
         call_dequeue().expect("bank_statements", 1),
         call_size().expect(0),
     ])
+
+def test_IWC_R2_S8() -> None:
+    run_queue([
+        call_enqueue("companies_house", 1, iso_ts(delta_minutes=2)).expect(1),
+        call_enqueue("id_verification", 1, iso_ts(delta_minutes=4)).expect(2),
+        # call_enqueue("credit_check", 1, iso_ts(delta_minutes=0)).expect(4),
+        call_dequeue().expect("companies_house", 1),
+        # call_dequeue().expect("credit_check", 1),
+        # call_dequeue().expect("companies_house", 1),
+        call_dequeue().expect("id_verification", 1),
+    ])
