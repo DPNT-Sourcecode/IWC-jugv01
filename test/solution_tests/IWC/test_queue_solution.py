dad5570eb1366403f2ea3307aebfea7b77b04ed5
sbx_user1051 <sbx_user1051@169.254.65.67>
2026-02-15 22:13:57 +0000

    Sun Feb 15 22:13:57 UTC 2026

--- test/solution_tests/IWC/test_queue_solution.py
+++ test/solution_tests/IWC/test_queue_solution.py
@@ -90,3 +90,15 @@ def test_resolve_deduplication_flow_replace_old_item() -> None:
         call_dequeue().expect("id_verification", 1),
         call_size().expect(0),
     ])
+
+def test_resolve_deduplication_flow_replace_old_item_with_dependencies() -> None:
+    run_queue([
+        call_enqueue("credit_check", 1, iso_ts(delta_minutes=5)).expect(2),
+        call_enqueue("bank_statements", 1, iso_ts(delta_minutes=7)).expect(3),
+        call_enqueue("credit_check", 1, iso_ts(delta_minutes=0)).expect(3),
+        call_size().expect(3),
+        call_dequeue().expect("companies_house", 1),
+        call_dequeue().expect("credit_check", 1),
+        call_dequeue().expect("companies_house", 1),
+        call_size().expect(0),
+    ])
