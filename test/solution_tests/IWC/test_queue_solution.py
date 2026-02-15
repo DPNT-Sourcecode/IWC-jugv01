e50ba45f67558729b521cd56e7968900daf3680b
sbx_user1051 <sbx_user1051@169.254.65.67>
2026-02-15 19:36:39 +0000

    Sun Feb 15 19:36:39 UTC 2026

--- test/solution_tests/IWC/test_queue_solution.py
+++ test/solution_tests/IWC/test_queue_solution.py
@@ -64,3 +64,13 @@ def test_dependency_resolution_added_dependencies_flow() -> None:
         call_dequeue().expect("credit_check", 1),
     ])
 
+def test_resolve_deduplication_dlow() -> None:
+    run_queue([
+        call_enqueue("bank_statements", 1, iso_ts(delta_minutes=0)).expect(1),
+        call_enqueue("bank_statements", 1, iso_ts(delta_minutes=5)).expect(1),
+        call_enqueue("id_verification", 1, iso_ts(delta_minutes=5)).expect(2),
+        call_size().expect(2),
+        call_dequeue().expect("bank_statements", 1),
+        call_dequeue().expect("id_verification", 1),
+        call_size().expect(0),
+    ])
