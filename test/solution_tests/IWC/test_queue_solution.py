aa7d4a8b2bfb37bd486fc0b0fd5fc01a6aafe2cf
sbx_user1051 <sbx_user1051@169.254.65.67>
2026-02-15 18:56:56 +0000

    Sun Feb 15 18:56:56 UTC 2026

--- test/solution_tests/IWC/test_queue_solution.py
+++ test/solution_tests/IWC/test_queue_solution.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from .utils import call_dequeue, call_enqueue, call_size, iso_ts, run_queue
+from utils import call_dequeue, call_enqueue, call_size, iso_ts, run_queue
 
 
 def test_enqueue_size_dequeue_flow() -> None:
@@ -9,3 +9,4 @@ def test_enqueue_size_dequeue_flow() -> None:
         call_size().expect(1),
         call_dequeue().expect("companies_house", 1),
     ])
+
