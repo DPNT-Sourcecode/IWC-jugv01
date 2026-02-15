1b5e37988114064f1403f748455584d2fc02563a
sbx_user1051 <sbx_user1051@169.254.65.67>
2026-02-15 18:15:51 +0000

    Sun Feb 15 18:15:51 UTC 2026

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -2,8 +2,10 @@
 class SumSolution:
     
     def compute(self, x: int, y: int) -> int:
+        """Returns the sum of two provided integers"""
         if not isinstance(x, int) or not isinstance(y, int):
             raise TypeError("Inputs must be integers")
         return x + y
+
 
 
