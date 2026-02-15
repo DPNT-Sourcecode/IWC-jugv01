5f127a4ec40ee7246740072d4120d046481ef1cb
sbx_user1051 <sbx_user1051@169.254.65.67>
2026-02-15 19:11:56 +0000

    Sun Feb 15 19:11:56 UTC 2026

--- test/solution_tests/IWC/test_queue_solution.py
+++ test/solution_tests/IWC/test_queue_solution.py
@@ -10,4 +10,23 @@ def test_enqueue_size_dequeue_flow() -> None:
         call_dequeue().expect("companies_house", 1),
     ])
 
+def test_enqueue_multiple_dequeue_single_flow() -> None:
+    run_queue([
+        call_enqueue("companies_house", 1, iso_ts(delta_minutes=0)).expect(1),
+        call_enqueue("bank_statements", 2, iso_ts(delta_minutes=0)).expect(2),
+        call_enqueue("id_verification", 1, iso_ts(delta_minutes=0)).expect(3),
+        call_enqueue("bank_statements", 1, iso_ts(delta_minutes=0)).expect(4),
+        call_size().expect(4),
+        call_dequeue().expect("companies_house", 1),
+    ])
 
+def test_enqueue_multiple_dequeue_multiple_flow() -> None:
+    run_queue([
+        call_enqueue("companies_house", 1, iso_ts(delta_minutes=0)).expect(1),
+        call_enqueue("bank_statements", 2, iso_ts(delta_minutes=0)).expect(2),
+        call_enqueue("id_verification", 1, iso_ts(delta_minutes=0)).expect(3),
+        call_enqueue("bank_statements", 1, iso_ts(delta_minutes=0)).expect(4),
+        call_size().expect(4),
+        call_dequeue().expect("companies_house", 1),
+        call_dequeue().expect("id_verification", 1),
+    ])
